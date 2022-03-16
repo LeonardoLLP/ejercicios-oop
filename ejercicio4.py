7a18d27cad3e4e2c4ca5583d5954f040bb3f7521
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-03-16 12:39:30 +0100

    Desarrollado ejercicio 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from inspect import ClassFoundException
+from typing import List
 
 
 class Dieta(Enum):
@@ -7,7 +9,7 @@ class Dieta(Enum):
     INSECTIVORO = 3
 
 
-class Animal:
+class Animal():
     def __init__(self, id, dieta, cuidador_actual):
         self.id = id
         self.dieta = list(dieta)
@@ -26,12 +28,48 @@ class Animal:
         print("Doing even more shit")
 
 
-class Empleado():
-    pass
-    
-
 class Vacaciones():
-    def __init__(self, inicio, duracion, empleado: Empleado):
+    def __init__(self, inicio, duracion, empleado):
         self.inicio = inicio
         self.duracion = duracion
         self.empleado = empleado
+
+
+class Cuidador():
+    pass
+    def __init__(self, nombre, apellido, animales: List[Animal]):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.animales = animales
+        self.vacaciones = None
+        self.animales = []
+
+    def solicitar_vacaciones(self, inicio, duracion):
+        self.vacaciones = Vacaciones(inicio, duracion, self)
+
+    def cuidar(self, animal: Animal):
+        self.animales.append(animal)
+
+    def dejar_de_cuidar(self, animal: Animal):
+        if animal in self.animales:
+            self.animales.remove(animal)
+        else
+            print("Ese animal no está en mi lista")
+
+    def alimentar(self, animal: Animal):
+        if animal in self.animales:
+            print("Alimentando a {}".format(animal.id))
+        else:
+            print("Yo no cuido a ese animal")
+
+
+class Comida():
+    # No tiene sentido lista de dietas: o es para herbivoros, o para carnivoros, o insectivoros.
+    # Tiene sentido lista de dietas en animales
+    def __init__(self, dieta):
+        self.dieta = dieta
+
+
+class Stock():
+    def __init__(self):
+        pass
